b72572ad29bfd24de1e72ad1f14b84f190697773
Marco Vásquez <marco_vasquez1@unitec.edu>
2026-03-13 11:43:50 -0600

    probando correciones

--- Lab7_binarios/src/lab7_binarios/Lab7_binarios.java
+++ Lab7_binarios/src/lab7_binarios/Lab7_binarios.java
@@ -17,6 +17,7 @@ public class Lab7_binarios {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         SwingUtilities.invokeLater(()->{
             new GUIReproductor().setVisible(true);
         });
